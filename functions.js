c719a3ca65dbf5fce7758a541841b72bdfeff824
suresh9058 <suresh9058@gmail.com>
2022-08-23 14:35:34 +0530

    Ashokit august month javascript basics repo.

---
+++ functions.js
@@ -0,0 +1,13 @@
+// console.log(sum(10,20));
+
+// function sum(a,b){
+// //  console.log(a+b);//a*c/d
+//  return a+b; // 30, 
+// }
+
+// function expression
+console.log(sum(10,20));// Uncaught ReferenceError: Cannot access 'sum' before initialization
+
+let sum = function(a,b) {
+    return a+b;
+}
---
+++ index.js
@@ -0,0 +1,63 @@
+// console.log("external file live server");
+// let user = {
+//     name: "rakesh",// key: value pair 
+//     age:32,
+//     let: 10,
+//     return: 20,
+//     "likes code": true
+// };
+// // let let=10;
+
+// console.log(user.let);// undefined
+// //  square notation
+// console.log(user["likes code"]);
+// // accessing the object properties
+// // dot notation
+// console.log(user.age); // 32
+// console.log(user.name); // rakesh
+
+// console.log(user);
+// // add properties to an object
+// user.isAdmin = false;
+// console.log(user);
+
+// // modify property in an object
+// user.age = 31;
+// console.log(user);
+
+// // remove or delete the property in an object
+// console.log(delete user.isAdmin);
+
+// console.log(user);
+
+// let a =10;
+// console.log(delete a);
+
+// Arrays
+// let fruits = ["apple","orange","plum"];
+
+// // how to access arrays
+// console.log(fruits[0]);
+// fruits[3]='kiwi';
+// console.log(fruits);
+// fruits.push("banana");
+// console.log(fruits);
+// fruits.pop();
+// console.log(fruits);
+
+// console.log(fruits.length); // 4
+
+// let fruits = [];
+// fruits[123] = "Apple";
+
+// console.log(fruits.length);// 2, 124, 
+
+// let fruits = ['apple', {name:"suresh"}, true, 10];
+
+// console.log(fruits);
+
+// // fruits.length = 0;
+// // console.log(fruits);
+
+// fruits.length = 2; //
+// console.log(fruits);
